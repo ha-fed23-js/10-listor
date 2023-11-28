9aa67d82995b341a44c486ebb17efb9dbb2a2804
David Andersson <lejonmanen@gmail.com>
2023-11-28 10:50:42 +0100

    Sortera drycker

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 import { drinks } from './data.js'
+import { compareByName, compareByPrice } from './sort.js'
 
 const drinkList = document.querySelector('.drink-list')
+const sortByName = document.querySelectorAll('.sort-by-name')
+const sortByPrice = document.querySelectorAll('.sort-by-price')
 
-const items = drinks.map(drink => {
-	const li = document.createElement('li')
-	const nameSpan = document.createElement('span')
-	const priceSpan = document.createElement('span')
-	nameSpan.innerText = drink.name
-	priceSpan.innerText = drink.price
-	li.append(nameSpan)
-	li.append(priceSpan)
-	// li.innerText = `${drink.name} ... ${drink.price}`
-	return li
+renderDrinkList(drinks)
+
+
+function renderDrinkList(list) {
+	drinkList.innerText = ''
+	const items = list.map(drink => {
+		const li = document.createElement('li')
+		const nameSpan = document.createElement('span')
+		const priceSpan = document.createElement('span')
+		nameSpan.innerText = drink.name
+		priceSpan.innerText = drink.price
+		li.append(nameSpan)
+		li.append(priceSpan)
+		// li.innerText = `${drink.name} ... ${drink.price}`
+		return li
+	})
+	items.forEach(item => {
+		drinkList.append(item)
+	})
+}
+
+
+
+sortByName.forEach(button => {
+	button.addEventListener('click', () => {
+		const sorted = drinks.toSorted(compareByName)
+		renderDrinkList(sorted)
+	})
 })
-items.forEach(item => {
-	drinkList.append(item)
+sortByPrice.forEach(button => {
+	button.addEventListener('click', () => {
+		const sorted = drinks.toSorted(compareByPrice)
+		renderDrinkList(sorted)
+	})
 })
